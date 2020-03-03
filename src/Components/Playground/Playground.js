3cdfef2f32497ff4eb0dcfd74f428a12909074ad
Arsh <arshdeepgary@gmail.com>
2020-03-02 19:31:37 -0800

    added react spring

--- src/Components/Playground/Playground.js
+++ src/Components/Playground/Playground.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 
 export default function Playground() {
+
+    const multiAnimation = useSpring({
+        from: { opacity: 0, color: 'red' },
+        to: [
+            { opacity: 1, color: '#ffaaee' },
+            { opacity: 1, color: 'red' },
+            { opacity: .5, color: '#008000' },
+            { opacity: .8, color: 'black' }
+        ]
+    });
+
     return(
-        <h2>Coming soon..</h2>
+        <div>
+      <animated.h1 style={multiAnimation}>Coming soon..</animated.h1>
+        </div>
     );
 }
