93b6a8b2166f607a39f30b22b43a247055a89d9a
Arsh <arshdeepgary@gmail.com>
2020-04-20 22:41:33 -0700

    style: added confetti

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import LandingPage from './Components/LandingPage/LandingPage';
-import Nav from './Components/NavBar/Nav';
-import Experience from './Components/Experience/Experience';
-import Playground from './Components/Playground/Playground';
-import './App.css';
-import { Link, Redirect } from 'react-router-dom';
-import { Router, Route } from 'react-router-dom';
+import React from "react";
+import LandingPage from "./Components/LandingPage/LandingPage";
+import Nav from "./Components/NavBar/Nav";
+import Experience from "./Components/Experience/Experience";
+import Playground from "./Components/Playground/Playground";
+import "./App.css";
+import { Link, Redirect } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 
 /********Material imports********/
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import { Hidden } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { Hidden } from "@material-ui/core";
 
 // Google Analytics
-import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
 ReactGA.initialize(process.env.REACT_APP_GA_KEY);
 
 const history = createBrowserHistory();
 
 // Initialize google analytics page view tracking
-history.listen(location => {
+history.listen((location) => {
   ReactGA.set({ page: location.pathname }); // Update the user's current page
   ReactGA.pageview(location.pathname); // Record a pageview for the given page
 });
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   nav: {
     height: "120px",
     backgroundColor: "#f1f1f1",
-    [theme.breakpoints.down('xs')]: {
+    [theme.breakpoints.down("xs")]: {
       height: "80px",
-      width:  "100vw"
-    }
-  }
+      width: "100vw",
+    },
+  },
 }));
 
 function App() {
@@ -48,7 +48,11 @@ function App() {
           <Hidden xsDown>
             <Grid item sm={4} className="topLogo">
               <Link to="/">
-                <img src={require("./images/myLogo.png")} alt="logo" className="logo" />
+                <img
+                  src={require("./images/myLogo.png")}
+                  alt="logo"
+                  className="logo"
+                />
               </Link>
             </Grid>
           </Hidden>
@@ -60,7 +64,7 @@ function App() {
       <Route exact path="/" component={LandingPage} />
       <Route exact path="/experience" component={Experience} />
       <Route exact path="/playground" component={Playground} />
-      <Redirect to='/' />
+      <Redirect to="/" />
     </Router>
   );
 }
--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,160 +1,243 @@
-import React from 'react';
-import './landingPage.css';
-import Mountain from '../SharedComponents/Mountain';
-import CenterText from '../SharedComponents/CenterText';
-import Footer from '../Footer/Footer';
-import AboutMe from '../LandingPage/AboutMe/AboutMe';
-/********Material imports********/
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import { Hidden } from '@material-ui/core';
+import React, { useEffect } from "react";
+import "./landingPage.css";
+import Mountain from "../SharedComponents/Mountain";
+import CenterText from "../SharedComponents/CenterText";
+import Footer from "../Footer/Footer";
+import AboutMe from "../LandingPage/AboutMe/AboutMe";
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-        overflow: "hidden",
+/********Material imports********/
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { Hidden } from "@material-ui/core";
+
+/********Confetti imports********/
+import ConfettiGenerator from "confetti-js";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    overflow: "hidden",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+  logo: {
+    width: "70px",
+    height: "70px",
+    paddingLeft: 100,
+    paddingTop: 6,
+  },
+  nav: {
+    height: "80px",
+    backgroundColor: "yellow",
+    [theme.breakpoints.down("xs")]: {
+      height: "60px",
     },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: "center",
-        color: theme.palette.text.secondary,
-    },
-    logo: {
-        width: "70px",
-        height: "70px",
-        paddingLeft: 100,
-        paddingTop: 6,
-    },
-    nav: {
-        height: "80px",
-        backgroundColor: "yellow",
-        [theme.breakpoints.down('xs')]: {
-            height: "60px",
-        }
-    },
-    midLeft: {
-        height: "55vh",
-        backgroundColor: "#f1f1f1",
-        textAlign: "center",
-        position: "relative",
-    },
-    midCentre: {
-        height: "55vh",
-        backgroundColor: "#f1f1f1",
-        textAlign: "center",
-        position: "relative",
-    },
-    midRight: {
-        height: "55vh",
-        backgroundColor: "#f1f1f1",
-        textAlign: "center",
-        position: "relative",
-    },
+  },
+  midLeft: {
+    height: "55vh",
+    backgroundColor: "#f1f1f1",
+    textAlign: "center",
+    position: "relative",
+  },
+  midCentre: {
+    height: "55vh",
+    backgroundColor: "#f1f1f1",
+    textAlign: "center",
+    position: "relative",
+  },
+  midRight: {
+    height: "55vh",
+    backgroundColor: "#f1f1f1",
+    textAlign: "center",
+    position: "relative",
+  },
 }));
 
 let handleBirdShoot = () => {
-    console.log("hey I am shot");
-}
+  console.log("hey I am shot");
+};
 
 export default function LandingPage() {
-    const classes = useStyles();
-
-    return (
-        <div className={classes.root}>
-            <div>
-                <div className="bird-container bird-container--one">
-                    <div className="bird bird--one"></div>
-                </div>
-                <div className="bird-container bird-container--three">
-                    <div className="bird bird--three"></div>
-                </div>
-                <div className="bird-container bird-container--four">
-                    <div className="bird bird--four"></div>
-                </div>
-            </div>
-            <Grid container spacing={0}>
-                <Hidden xsDown>
-                    <Grid item xs={12} sm={3} className={classes.midLeft}>
-                        <img src={require("../../images/cloud.svg")} alt="logo" className="cloudLeft" />
-                    </Grid>
-                </Hidden>
-                <Grid item xs={12} sm={6} className={classes.midCentre}>
-                    {/* <img src={require("../../images/iamarsh.png")} alt="logo" className="centePiece"/> */}
-                    <CenterText />
-                </Grid>
-                <Hidden xsDown>
-                    <Grid item xs={12} sm={3} className={classes.midRight}>
-                        <img src={require("../../images/cloud.svg")} alt="logo" className="cloudRight" />
-                    </Grid>
-                </Hidden>
-                <Grid item xs={12} className="bottom">
-                    <Mountain bgColor="#45b29a" className="firstMountain" zindex="4" />
-                    <div>
-                        <img src={require("../../images/house2.png")} alt="house2"
-                            style={{
-                                height: 55, width: 50, zIndex: 4, position: "absolute",
-                                marginTop: 70, marginLeft: 0,
-                            }} />
-                    </div>
-                    <div>
-                        <img src={require("../../images/house1.png")} alt="house1"
-                            style={{
-                                height: 40, width: 40, marginTop: 16,
-                                marginLeft: 192, position: "absolute"
-                            }} />
-                    </div>
-                    <div>
-                        <img src={require("../../images/tree4.png")} alt="tree"
-                            style={{
-                                height: 65, width: 60, position: "absolute", marginTop: 100,
-                                marginLeft: -62, zIndex: 4
-                            }} />
-                    </div>
-                    <div>
-                        <img src={require("../../images/tree.png")} alt="tree"
-                            style={{
-                                height: 65, width: 50, position: "absolute", marginTop: 138,
-                                marginLeft: -96, zIndex: 4
-                            }} />
-                    </div>
-                    <div>
-                        <img src={require("../../images/tree4.png")} alt="tree"
-                            style={{
-                                height: 75, width: 70, position: "absolute", marginTop: 175,
-                                marginLeft: -146, zIndex: 4
-                            }} />
-                    </div>
-                    <div>
-                        <img src={require("../../images/tree.png")} alt="tree"
-                            style={{
-                                height: 30, width: 25, position: "absolute", marginTop: 38,
-                                marginLeft: 244, zIndex: 4, filter: "contrast(100%)",
-                            }} />
-                    </div>
-                    <div>
-                        <img src={require("../../images/tree.png")} alt="tree"
-                            style={{
-                                height: 40, width: 30, position: "absolute", marginTop: 65,
-                                marginLeft: 130, zIndex: 4, filter: "contrast(100%)",
-                            }} />
-                    </div>
-                    <div>
-                        <img src={require("../../images/panel.png")} alt="direction"
-                            style={{
-                                height: 35, width: 35, position: "absolute", marginTop: 75,
-                                marginLeft: 352, zIndex: 4
-                            }} />
-                    </div>
-                    <Mountain bgColor="#328a76" className="secondMountain" zindex="2" />
-                    <Mountain bgColor="white" className="thirdMountain" zindex="3" />
-                </Grid>
-                <Grid item xs={12} className="secondTop">
-                    <AboutMe />
-                </Grid>
-                <Grid item xs={12} className="footer" style={{ marginTop: 20 }}>
-                    <Footer />
-                </Grid>
-            </Grid>
-        </div >
-    );
+  const classes = useStyles();
+  useEffect(() => {
+    const confettiSettings = {
+      target: "confetti-canvas",
+      max: 15,
+      clock: 5,
+      rotate: true,
+    };
+    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+    return () => confetti.clear();
+  }, []);
+  return (
+    <div className={classes.root}>
+      <canvas
+        id="confetti-canvas"
+        style={{ position: "absolute", zIndex: 1, opacity: "1" }}
+      ></canvas>
+      <div>
+        <div className="bird-container bird-container--one">
+          <div className="bird bird--one"></div>
+        </div>
+        <div className="bird-container bird-container--three">
+          <div className="bird bird--three"></div>
+        </div>
+        <div className="bird-container bird-container--four">
+          <div className="bird bird--four"></div>
+        </div>
+      </div>
+      <Grid container spacing={0}>
+        <Hidden xsDown>
+          <Grid item xs={12} sm={3} className={classes.midLeft}>
+            <img
+              src={require("../../images/cloud.svg")}
+              alt="logo"
+              className="cloudLeft"
+            />
+          </Grid>
+        </Hidden>
+        <Grid item xs={12} sm={6} className={classes.midCentre}>
+          {/* <img src={require("../../images/iamarsh.png")} alt="logo" className="centePiece"/> */}
+          <CenterText />
+        </Grid>
+        <Hidden xsDown>
+          <Grid item xs={12} sm={3} className={classes.midRight}>
+            <img
+              src={require("../../images/cloud.svg")}
+              alt="logo"
+              className="cloudRight"
+            />
+          </Grid>
+        </Hidden>
+        <Grid item xs={12} className="bottom">
+          <Mountain bgColor="#45b29a" className="firstMountain" zindex="4" />
+          <div>
+            <img
+              src={require("../../images/house2.png")}
+              alt="house2"
+              style={{
+                height: 55,
+                width: 50,
+                zIndex: 4,
+                position: "absolute",
+                marginTop: 70,
+                marginLeft: 0,
+              }}
+            />
+          </div>
+          <div>
+            <img
+              src={require("../../images/house1.png")}
+              alt="house1"
+              style={{
+                height: 40,
+                width: 40,
+                marginTop: 16,
+                marginLeft: 192,
+                position: "absolute",
+              }}
+            />
+          </div>
+          <div>
+            <img
+              src={require("../../images/tree4.png")}
+              alt="tree"
+              style={{
+                height: 65,
+                width: 60,
+                position: "absolute",
+                marginTop: 100,
+                marginLeft: -62,
+                zIndex: 4,
+              }}
+            />
+          </div>
+          <div>
+            <img
+              src={require("../../images/tree.png")}
+              alt="tree"
+              style={{
+                height: 65,
+                width: 50,
+                position: "absolute",
+                marginTop: 138,
+                marginLeft: -96,
+                zIndex: 4,
+              }}
+            />
+          </div>
+          <div>
+            <img
+              src={require("../../images/tree4.png")}
+              alt="tree"
+              style={{
+                height: 75,
+                width: 70,
+                position: "absolute",
+                marginTop: 175,
+                marginLeft: -146,
+                zIndex: 4,
+              }}
+            />
+          </div>
+          <div>
+            <img
+              src={require("../../images/tree.png")}
+              alt="tree"
+              style={{
+                height: 30,
+                width: 25,
+                position: "absolute",
+                marginTop: 38,
+                marginLeft: 244,
+                zIndex: 4,
+                filter: "contrast(100%)",
+              }}
+            />
+          </div>
+          <div>
+            <img
+              src={require("../../images/tree.png")}
+              alt="tree"
+              style={{
+                height: 40,
+                width: 30,
+                position: "absolute",
+                marginTop: 65,
+                marginLeft: 130,
+                zIndex: 4,
+                filter: "contrast(100%)",
+              }}
+            />
+          </div>
+          <div>
+            <img
+              src={require("../../images/panel.png")}
+              alt="direction"
+              style={{
+                height: 35,
+                width: 35,
+                position: "absolute",
+                marginTop: 75,
+                marginLeft: 352,
+                zIndex: 4,
+              }}
+            />
+          </div>
+          <Mountain bgColor="#328a76" className="secondMountain" zindex="2" />
+          <Mountain bgColor="white" className="thirdMountain" zindex="3" />
+        </Grid>
+        <Grid item xs={12} className="secondTop">
+          <AboutMe />
+        </Grid>
+        <Grid item xs={12} className="footer" style={{ marginTop: 20 }}>
+          <Footer />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
